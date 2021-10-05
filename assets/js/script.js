2ee6e550a4f9cb7d3117f77e85022b0bfe87324a
Mark L-M <mark.lohsemiranda@gmail.com>
2021-10-04 20:26:45 -0700

    finished cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,124 +3,145 @@ var lowerEl = $("#lower");
 var fullEl = $("#full");
 var createEl = $("#create");
 var typeWorkoutEl = $("#type-workout");
-var searchInput = $('#text-bar')
+var searchInput = $("#text-bar");
 var exerciseList;
-var test;
-<<<<<<< HEAD
+var test=[];
 var upperBody;
 var lowerBody;
-
-=======
-var exEl = $('.ex');
-var chosenListEl = $('#chosen-list');
->>>>>>> dev
+var chosenListEl = $("#chosen-list");
+var exerciseListOjbect 
+var setsEl = $(".sets");
+var repsEl = $(".reps");
 
 // gets all data from API and saves it locally
 
 function getAllExercises() {
-    fetch("https://exercisedb.p.rapidapi.com/exercises", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-            "x-rapidapi-key": "8605916b99mshaa4c5335b3c5676p1b53a9jsn004d4aba7c60"
-        }
-    })
-    .then(response => {
-        return response.json();
+  fetch("https://exercisedb.p.rapidapi.com/exercises", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+      "x-rapidapi-key": "8605916b99mshaa4c5335b3c5676p1b53a9jsn004d4aba7c60",
+    },
+  })
+    .then((response) => {
+      return response.json();
     })
     .then(function (data) {
-        console.log('got API'); // here temp to let me know the API was got
-        localStorage.setItem('allExercises', JSON.stringify(data));
-        exerciseList = data;
+      console.log("got API"); // here temp to let me know the API was got
+      localStorage.setItem("allExercises", JSON.stringify(data));
+      exerciseList = data;
     })
-    .catch(err => {
-        console.error(err);
-    }); 
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
 // if data is already local get that, if not get API
 
 function localOrApi() {
-    exerciseList = JSON.parse(localStorage.getItem('allExercises'));
-    if (exerciseList == null) {
-        getAllExercises();
-    } 
+  exerciseList = JSON.parse(localStorage.getItem("allExercises"));
+  if (exerciseList == null) {
+    getAllExercises();
+  }
 }
 
 localOrApi();
 
 // this is a test search returning exercises that meet the lised conditions
 
-function generateCard(testsearch) {
-    test = [];
-    for (i=0; i < exerciseList.length; i++) {
-        if (exerciseList[i].name === testsearch) { 
-        test.push(exerciseList[i])
-        }
+function generateListByName(name) {
+//   test = [];
+  for (i = 0; i < exerciseList.length; i++) {
+    if (exerciseList[i].name === name) {
+      test.push(exerciseList[i]);
+      var temp = test.length - 1;
+      test[temp].reps = 1;
+      test[temp].sets = 1;
     }
-    var pic = test[0].gifUrl;
-    chosenListEl.append('<li class="ex"><div>name: ' + test[0].name + '</div><div>Equipment: ' + test[0].equipment + '</div><div class="hide"><img style=width:100px;height:100px" src="' + pic + '"></div>');
+  }
 }
 
-// upperEl.click(search('dumbbell bench press'));
+function buildCardsFromList() {
+    for (i = 0; i < test.length; i++) {
+          var pic = test[i].gifUrl;
+          chosenListEl.append(
+            '<li class="ex ui-state-default">' + 
+            '<button data-pos="' + i + '">X</button>' + 
+            '<div class="pure-g">' +
+            '<div class="sidebar pure-u-1-3">' +
+            '<div style="text-align:center;font-size: .5vw;">Click Below</div>' +
+            '<img class="hide" style=width:100%;height:auto;" src="' +
+            pic + '"></div>' +
+            '<div class="content pure-u-2-3">' +
+            '<p>Name: ' + test[i].name + '</p>' +
+            '<p>Muscle target: ' + test[i].target + '</p>' +
+            '<p>Sets: ' + 
+            '<input type="number" data-pos="' + i + '" class="sets" min="1" value="' + test[i].sets + '"></p>' +
+            '<p>Reps: ' +
+            '<input type="number" data-pos="' + i + '" class="reps" min="1" value="' + test[i].reps + '"></p>' +
+            '<p>Equipment: ' + test[i].equipment + '</p>' +
+            '</div></div></li>')
+        
+    }
+}
 
 // change color when clicked
 
-createEl.on('click', function() {
-    createEl.css('background-color', 'darkgoldenrod');
-    upperEl.css('background-color', '');
-    lowerEl.css('background-color', '');
-    fullEl.css('background-color', '');
-})
-
-upperEl.on('click', function() {
-    createEl.css('background-color', '');
-    upperEl.css('background-color', 'darkgoldenrod');
-    lowerEl.css('background-color', '');
-    fullEl.css('background-color', '');
-    chosenListEl.html('');
-    generateCard('dumbbell bench press');
-    generateCard('dumbbell biceps curl')
-
-})
-
-lowerEl.on('click', function() {
-    createEl.css('background-color', '');
-    upperEl.css('background-color', '');
-    lowerEl.css('background-color', 'darkgoldenrod');
-    fullEl.css('background-color', '');
-})
-
-fullEl.on('click', function() {
-    createEl.css('background-color', '');
-    upperEl.css('background-color', '');
-    lowerEl.css('background-color', '');
-    fullEl.css('background-color', 'darkgoldenrod');
-})
-
-$(function () {
-    $('#chosen-list').sortable();
+createEl.on("click", function () {
+  createEl.css("background-color", "darkgoldenrod");
+  upperEl.css("background-color", "");
+  lowerEl.css("background-color", "");
+  fullEl.css("background-color", "");
 });
 
+upperEl.on("click", function () {
+  createEl.css("background-color", "");
+  upperEl.css("background-color", "darkgoldenrod");
+  lowerEl.css("background-color", "");
+  fullEl.css("background-color", "");
+  chosenListEl.html("");
+  test=[];
+  generateListByName("dumbbell bench press");
+  generateListByName("dumbbell biceps curl");
+  generateListByName("dumbbell bent over row");
+  generateListByName("dumbbell seated shoulder press");
+  generateListByName("dumbbell seated triceps extension");
+  buildCardsFromList();
+});
 
+lowerEl.on("click", function () {
+  createEl.css("background-color", "");
+  upperEl.css("background-color", "");
+  lowerEl.css("background-color", "darkgoldenrod");
+  fullEl.css("background-color", "");
+});
 
+fullEl.on("click", function () {
+  createEl.css("background-color", "");
+  upperEl.css("background-color", "");
+  lowerEl.css("background-color", "");
+  fullEl.css("background-color", "darkgoldenrod");
+});
 
-chosenListEl.on('mouseleave', '.ex', function(event) {   
-    event.stopPropagation();
-    var imgEl = $(this).find(':nth-child(3)');
-    imgEl.toggleClass('hide');
-    
-})
+$(function () {
+  $("#chosen-list").sortable();
+});
 
-chosenListEl.on('mouseenter', '.ex', function(event) {   
-    event.stopPropagation();
-    var imgEl = $(this).find(':nth-child(3)');
-    imgEl.toggleClass('hide');
-    
-})
+chosenListEl.on("click", ".sidebar", function (event) {
+  event.stopPropagation();
+  $(this).find("img").toggleClass("hide");
+  
+});
 
-chosenListEl.on('click', '.ex', function(event) {
-    event.stopPropagation();
-    $(this).toggleClass('overlay');  
-    
-})
+chosenListEl.on('click', 'button', function() {
+    test.splice($(this).data('pos'), 1);
+    $(this).parent().remove();
+});
+
+chosenListEl.on('click', '.sets', function() {
+    test[$(this).data('pos')].sets = $(this).val();  
+});
+
+chosenListEl.on('click', '.reps', function() {
+    test[$(this).data('pos')].reps = $(this).val();    
+});
